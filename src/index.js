2abf972274b1294cbb27fc8f5eb9b48f49351e5b
lmle <lymaile@gmail.com>
2020-12-20 11:53:43 -0800

    Add eslint

--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ const uniqBy = require('lodash.uniqby');
 const { PETHARBOR_SPECIAL_NEEDS_DOGS_URL, PETHARBOR_DOMAIN } = require('./constants/petharbor');
 const dogKeys = require('./constants/dogKeys');
 const getCurrentISO = require('./utils/getCurrentISO');
-let previousDogs = [];
+
 const app = express();
+let previousDogs = [];
 
 app.get('/', (req, res) => {
   res.send('Bark bark, doggy-mon is running.');
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`doggy-mon port ${process.env.PORT}`);
+  console.log(`doggy-mon port ${process.env.PORT}`); // eslint-disable-line no-console
 });
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -21,6 +22,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const monitorDogs = async () => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
+  const $ = null; // for eslint
 
   await page.goto(PETHARBOR_SPECIAL_NEEDS_DOGS_URL);
 
@@ -37,29 +39,22 @@ const monitorDogs = async () => {
 
     const dogs = [];
 
-    $('.ResultsTable tr').each(function(trIndex) {
+    $('.ResultsTable tr').each(function getDogs(trIndex) {
       if (trIndex === 0) return;
 
       const dog = {};
 
-      $(this).find('td').each(function(i) {
+      $(this).find('td').each(function pushDog(i) {
         const key = tdMap[i];
 
         if (key === keys.IMAGE_URL) {
           dog[keys.IMAGE_URL] = `${petharborDomain}/${$(this).find('img').attr('src')}`;
           dog[keys.ID] = (new URLSearchParams($(this).find('a').attr('href').split('?')[1])).get('ID');
           dog[keys.URL] = `${petharborDomain}/pet.asp?uaid=SNJS.${dog[keys.ID]}`;
-          return;
-        }
-
-        if (key === keys.DATE) {
+        } else if (key === keys.DATE) {
           dog[keys.DATE] = $(this).find('span').text();
-          return;
-        }
-
-        if (key) {
+        } else if (key) {
           dog[key] = $(this).text();
-          return;
         }
       });
 
@@ -70,27 +65,25 @@ const monitorDogs = async () => {
   }, dogKeys, PETHARBOR_DOMAIN);
 
   const newDogs = currentDogs
-    .filter((currentDog) => {
-      return !previousDogs.find((previousDog) => previousDog[dogKeys.ID] === currentDog[dogKeys.ID]);
-    })
+    .filter((c) => !previousDogs.find((p) => p[dogKeys.ID] === c[dogKeys.ID]))
     .reverse();
 
   if (newDogs.length) {
     previousDogs = currentDogs;
-    const subject = `New Dogs: ${uniqBy(newDogs.map((dog) => dog[dogKeys.BREED]), (breed) => breed.toLowerCase()).join(' | ')}`
+    const subject = `New Dogs: ${uniqBy(newDogs.map((dog) => dog[dogKeys.BREED]), (breed) => breed.toLowerCase()).join(' | ')}`;
 
-    const html = newDogs.map((dog) => {
-      return (
-        `<p>` +
-          `<img src='${dog[dogKeys.IMAGE_URL]}' />` +
-          `<br />` +
-          `<a href='${dog[dogKeys.URL]}'>${dog[dogKeys.NAME]}</a>` +
-          ['id', 'sex', 'color', 'breed', 'age', 'date'].map((key) => `<br /><b>${key}:</b> ${dog[key]}`).join('') +
-          `<br />` +
-          `<a href='mailto:SJAdopt@sanjoseca.gov?subject=${encodeURIComponent(`Interested in ${dog[dogKeys.ID]}`)}&body=${encodeURIComponent(`Hello, I am interested in ${dog[dogKeys.ID]}. Please let me know if I can meet him/her. Thank you!`)}'>Send email</a>` +
-        `</p>`
-      );
-    }).join('');
+    const html = newDogs
+      .map((dog) => (`
+        <p>
+          <img src='${dog[dogKeys.IMAGE_URL]}' />
+          <br />
+          <a href='${dog[dogKeys.URL]}'>${dog[dogKeys.NAME]}</a>
+          ${['id', 'sex', 'color', 'breed', 'age', 'date'].map((key) => `<br /><b>${key}:</b> ${dog[key]}`).join('')}
+          <br />
+          <a href='mailto:SJAdopt@sanjoseca.gov?subject=${encodeURIComponent(`Interested in ${dog[dogKeys.ID]}`)}&body=${encodeURIComponent(`Hello, I am interested in ${dog[dogKeys.ID]}. Please let me know if I can meet him/her. Thank you!`)}'>Send email</a>
+        </p>
+      `))
+      .join('');
 
     const msg = {
       to: process.env.SENDGRID_TO_EMAIL,
@@ -101,10 +94,10 @@ const monitorDogs = async () => {
 
     try {
       await sgMail.send(msg);
-      console.info(`${getCurrentISO()} - monitorDogs - email sent`);
+      console.info(`${getCurrentISO()} - monitorDogs - email sent`); // eslint-disable-line no-console
     } catch (error) {
-      console.error(`${getCurrentISO()} - monitorDogs - email error`);
-      console.error(error);
+      console.error(`${getCurrentISO()} - monitorDogs - email error`); // eslint-disable-line no-console
+      console.error(error); // eslint-disable-line no-console
     }
   }
 
@@ -122,10 +115,10 @@ const sendAppHealthEmail = async () => {
 
   try {
     await sgMail.send(msg);
-    console.info(`${getCurrentISO()} - sendAppHealthEmail - email sent`);
+    console.info(`${getCurrentISO()} - sendAppHealthEmail - email sent`); // eslint-disable-line no-console
   } catch (error) {
-    console.error(`${getCurrentISO()} - sendAppHealthEmail - email error`);
-    console.error(error);
+    console.error(`${getCurrentISO()} - sendAppHealthEmail - email error`); // eslint-disable-line no-console
+    console.error(error); // eslint-disable-line no-console
   }
 };
 
